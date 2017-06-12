ca8ebaa9f8aa868c61a5f4612d9e7edfa9d9ff9a
ganeshp <ganesh_pachpind@external.mckinsey.com>
2017-06-12 23:15:05 +0530

    Adding topic model with upVote and downvote functionality

---
+++ app/src/main/java/model/Topic.java
@@ -0,0 +1,32 @@
+package model;
+
+
+public class Topic {
+    private String content;
+    private long upVoteCount;
+    private long downVoteCount;
+
+    public Topic(String content) {
+        this.content = content;
+    }
+
+    public void upVote() {
+        upVoteCount++;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getUpVoteCount() {
+        return upVoteCount;
+    }
+
+    public long getDownVoteCount() {
+        return downVoteCount;
+    }
+
+    public void downVote() {
+        downVoteCount++;
+    }
+}
---
+++ app/src/test/java/model/TopicTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TopicTest {
+    @Test
+    public void shouldIncrementUpVoteCountWhenUpVote() throws Exception {
+        Topic topic = new Topic("Some content");
+
+        topic.upVote();
+
+        assertThat(topic.getUpVoteCount(), is(1L));
+        assertThat(topic.getDownVoteCount(), is(0L));
+    }
+
+    @Test
+    public void shouldIncrementDownVoteCountWhenDownVote() throws Exception {
+        Topic topic = new Topic("Some content");
+
+        topic.downVote();
+
+        assertThat(topic.getDownVoteCount(), is(1L));
+        assertThat(topic.getUpVoteCount(), is(0L));
+    }
+}
